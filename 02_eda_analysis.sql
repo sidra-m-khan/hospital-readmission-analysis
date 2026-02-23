5f65f09ee61980f18d1af90caf6388dd451ce058
Sidra Khan <sidramohsin610@gmail.com>
2026-02-23 11:25:31 +0100

    Add comments and improve SQL query formatting

--- 02_eda_analysis.sql
+++ 02_eda_analysis.sql
@@ -6,17 +6,23 @@
 
 
 -- Distribution of patients by age group
+-- Description: Calculate total patients for age group
 
 SELECT age_group, COUNT(*)
 FROM diabetes_clean
 GROUP BY age_group;
 
 -- Overall readmission distribution
+-- Description: Calculate total patients which are readmitted
+
 SELECT readmitted, COUNT(*)
 FROM diabetes_clean
 GROUP BY readmitted;
 
 -- Count by Gender
+-- Calculate total patientd by Gender
+
 SELECT GENDER, COUNT(*) AS patient_count
 FROM diabetes_clean
+
 GROUP BY GENDER;
